269d0f40f3cd85a72c73d7cbd3e1b616ae4c2be0
Sandy25121 <santhoshkumarachiver@gmail.com>
2024-09-12 09:48:31 +0530

    redbus.py

--- redbus.py
+++ redbus.py
@@ -1,6 +1,4 @@
 #Streamlit installation
-pip install streamlit
-
 import streamlit as st
 import pymysql as mysql
 import pandas as pd
@@ -10,9 +8,22 @@ def get_db_connection():
     return mysql.connect(
         host='127.0.0.1',
         user='root',
-        password='@@@@@@',
+        password='@@@@@',
         database='redbus_application'
     )
+
+# Insert user data into the database
+def insert_user_data(name, email):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    # Insert query
+    insert_query = "INSERT INTO user_details (name, email) VALUES (%s, %s)"
+    cursor.execute(insert_query, (name, email))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 # Data fetch filter
 def fetch_data(filters, table):
@@ -22,7 +33,6 @@ def fetch_data(filters, table):
     # Apply filters
     if filters.get('route_name') and filters['route_name'] != 'No Value':
         query += f" AND route_name LIKE '%{filters['route_name']}%'"
-    
     
     if filters.get('bus_type'):
         bus_types = filters['bus_type']
@@ -64,69 +74,96 @@ def get_route_names(table):
     conn.close()
     return df['route_name'].tolist()
 
-# Title
-st.title('Red Bus Application')
 
-# Sidebar menu
-st.sidebar.title('Main Menu')
-if st.sidebar.button('Home'):
-    st.experimental_rerun()
+# Store user form submission state
+if 'submitted' not in st.session_state:
+    st.session_state['submitted'] = False
 
-selected_option = st.sidebar.selectbox('Select Option', ['Home', 'Select Buses'])
+# Home page: Get user details
+if not st.session_state['submitted']:
+    st.title('Welcome to Red Bus')
 
-if selected_option == 'Home':
-    st.title('🏠Home Page')
-    st.write('Welcome to the Home Page!')
-    st.write('RedBus is India’s largest online bus ticketing platform that has transformed bus travel in the country by bringing ease and convenience to millions of Indians who travel using buses. Founded in 2006, redBus is part of India’s leading online travel company MakeMyTrip Limited (NASDAQ: MMYT). By providing widest choice, superior customer service, lowest prices and unmatched benefits, redBus has served over 18 million customers. redBus has a global presence with operations across Indonesia, Singapore, Malaysia, Colombia and Peru apart from India.')
+    with st.form("user_details_form"):
+        Name = st.text_input('Enter your name')
+        Email = st.text_input('Enter your email address')
 
-elif selected_option == 'Select Buses':
-    st.title('Select Buses')
-    
-    # Sidebar filters
-    st.sidebar.header('Filter Options')
-    table_name = st.sidebar.selectbox('Select the state buses name', 
-                                      ['apsrtc', 'ksrtc', 'bsrtc', 'rsrtc', 'tsrtc', 'ktcl', 'kaac', 'wbtc', 'pepsu', 'nbstc'])
-    
-    # fetch route names
-    route_names = get_route_names(table_name)
-    route_name = st.sidebar.selectbox('Select the route name', ['No Value'] + route_names)
-    
-    #  bus type multiselect
-    bus_type = st.sidebar.multiselect('Select the bus type', 
-                                       ['A.C', 'A/C ', 'AC ', 'Non A.C', 'Non AC', 'Non A/C', 'Sleeper', 'Seater'])
-    
-    # starting time filter with 24-hour format ranges
-    starting_time = st.sidebar.selectbox('Select the departing time', 
-                                         ['No Value', '00:00 to 01:00', '01:00 to 02:00', '02:00 to 03:00', '03:00 to 04:00', 
-                                          '04:00 to 05:00', '05:00 to 06:00', '06:00 to 07:00', '07:00 to 08:00', 
-                                          '08:00 to 09:00', '09:00 to 10:00', '10:00 to 11:00', '11:00 to 12:00', 
-                                          '12:00 to 13:00', '13:00 to 14:00', '14:00 to 15:00', '15:00 to 16:00', 
-                                          '16:00 to 17:00', '17:00 to 18:00', '18:00 to 19:00', '19:00 to 20:00', 
-                                          '20:00 to 21:00', '21:00 to 22:00', '22:00 to 23:00', '23:00 to 24:00'])
-    
-    #  Rating filter dropdown
-    rating = st.sidebar.selectbox('Select the rating', 
-                                  ['No Value', '1 to 2', '2 to 3', '3 to 4', '4 to 5'])
-    
-    # Updated bus fare input
-    bus_fare = st.sidebar.selectbox('Select the bus fare', 
-                                    ['No Value', '0 to 500', '500 to 1000', '1000 to 1500', '1500 to 2000', 
-                                     '2000 to 2500', '2500 to 3000', '3500 to 4000', '5000 to 6000'])
-    
-    filters = {
-        'route_name': route_name,
-        'bus_type': bus_type,
-        'starting_time': starting_time,
-        'rating': rating,
-        'bus_fare': bus_fare
-    }
+        # Handle form submission
+        submitted = st.form_submit_button('Submit')
+        if submitted:
+            if Name and Email:
+                # Store user data in the database
+                insert_user_data(Name, Email)
+                st.session_state['submitted'] = True
+                st.session_state['Name'] = Name
+                st.session_state['Email'] = Email
+                st.success("Your details have been successfully saved!")
+                st.experimental_rerun()
+            else:
+                st.error("Please fill in both your name and email.")
 
-    # Filters
-    st.header(f"{table_name.upper()} Buses")
-    bus_data = fetch_data(filters, table_name)
-    st.dataframe(bus_data)
+# Once user has submitted the form, show bus selection options
+if st.session_state['submitted']:
+    st.sidebar.title('Main Menu')
 
+    selected_option = st.sidebar.radio('Options', ['Home', 'Select Buses'])
+
+    if selected_option == 'Home':
+        # Retrieve Name from session state
+        st.title(f'Howdy, {st.session_state["Name"]}!')
+        st.image('C:/Users/santh/OneDrive/Documents/Redbus/Redbusq.jpg', width=125)
+        st.write('### Welcome to Red Bus!')
+        st.write('RedBus is India’s largest online bus ticketing platform that has transformed bus travel in the country by bringing ease and convenience to millions of Indians who travel using buses. Founded in 2006, redBus is part of India’s leading online travel company MakeMyTrip Limited (NASDAQ: MMYT). By providing widest choice, superior customer service, lowest prices and unmatched benefits, redBus has served over 18 million customers. redBus has a global presence with operations across Indonesia, Singapore, Malaysia, Colombia and Peru apart from India.')
+        st.write('## How to book bus tickets')
+        st.video('https://www.youtube.com/watch?v=eyAAUGhvZu8')
+
+    elif selected_option == 'Select Buses':
+        st.title('Select Buses')
+
+        # Sidebar filters
+        st.sidebar.header('Filter Options')
+        table_name = st.sidebar.selectbox('Select the state buses name', 
+                                          ['apsrtc', 'ksrtc', 'bsrtc', 'rsrtc', 'tsrtc', 'ktcl', 'kaac', 'wbtc', 'pepsu', 'nbstc'])
+
+        # fetch route names
+        route_names = get_route_names(table_name)
+        route_name = st.sidebar.selectbox('Select the route name', ['No Value'] + route_names)
+
+        # bus type multiselect
+        bus_type = st.sidebar.multiselect('Select the bus type', 
+                                          ['AC Seater',  'AC Sleeper', 'Non AC Seater', 'Non AC Sleeper'])
+
+        # starting time filter with 24-hour format ranges
+        starting_time = st.sidebar.selectbox('Select the departing time', 
+                                             ['No Value', '00:00 to 01:00', '01:00 to 02:00', '02:00 to 03:00', '03:00 to 04:00', 
+                                              '04:00 to 05:00', '05:00 to 06:00', '06:00 to 07:00', '07:00 to 08:00', 
+                                              '08:00 to 09:00', '09:00 to 10:00', '10:00 to 11:00', '11:00 to 12:00', 
+                                              '12:00 to 13:00', '13:00 to 14:00', '14:00 to 15:00', '15:00 to 16:00', 
+                                              '16:00 to 17:00', '17:00 to 18:00', '18:00 to 19:00', '19:00 to 20:00', 
+                                              '20:00 to 21:00', '21:00 to 22:00', '22:00 to 23:00', '23:00 to 24:00'])
+
+        # Rating filter dropdown
+        rating = st.sidebar.selectbox('Select the rating', 
+                                      ['No Value', '1 to 2', '2 to 3', '3 to 4', '4 to 5'])
+
+        # Updated bus fare input
+        bus_fare = st.sidebar.selectbox('Select the bus fare', 
+                                        ['No Value', '0 to 500', '500 to 1000', '1000 to 1500', '1500 to 2000', 
+                                         '2000 to 2500', '2500 to 3000', '3500 to 4000', '5000 to 6000'])
+
+        filters = {
+            'route_name': route_name,
+            'bus_type': bus_type,
+            'starting_time': starting_time,
+            'rating': rating,
+            'bus_fare': bus_fare
+        }
+
+        # Filters
+        st.header(f"{table_name.upper()} Buses")
+        bus_data = fetch_data(filters, table_name)
+        st.dataframe(bus_data)
+    
 # Add footer
-st.sidebar.info("Red Bus Application.")
+st.sidebar.markdown("[Red Bus Application](https://www.redbus.in/)")
 
 
